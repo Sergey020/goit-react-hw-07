97da980b4dfecb904d18556ede40eb62c1c0e77e
Sergey020 <114489697+Sergey020@users.noreply.github.com>
2024-07-06 12:23:04 +0300

    delete persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./rootReducer";
-import storage from "redux-persist/lib/storage";
-import persistReducer from "redux-persist/es/persistReducer";
-import persistStore from "redux-persist/es/persistStore";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  whitelist: ["contacts"],
-};
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
-      },
-    }),
+  reducer: rootReducer
 });
 
-export const persistor = persistStore(store);
